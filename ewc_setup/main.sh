cd744a4f453d30dc0fb654508faf4a085c4f3d0c
rruefenacht <rruefenacht@users.noreply.github.com>
2023-11-20 15:57:16 +0100

    add main routine doing the entire VM setup

---
+++ ewc_setup/main.sh
@@ -0,0 +1,18 @@
+# This is the main script orchestrating the whole set up of a new VM from scratch
+
+path_scripts=$(dirname "$0")
+
+echo 
+echo "setting up new VM from scratch"
+echo "=============================="
+echo
+
+chmod a+x $path_scripts/*.sh  # make sure all bash scripts are executable
+
+$path_scripts/update_python.sh
+$path_scripts/install_mwr_l12l2.sh
+$path_scripts/install_s3.sh
+$path_scripts/s3_create_buckets.sh eprofile-dl-raw eprofile-dl-l1  # comment this out if buckets alredy exist
+$path_scripts/s3_automount_buckets.sh eprofile-dl-raw eprofile-dl-l1
+
+
