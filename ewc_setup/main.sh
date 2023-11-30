b2c7e656040a8ede6ebc1ffeffb170a5aee657c8
rruefenacht <rruefenacht@users.noreply.github.com>
2023-11-30 16:16:27 +0100

    check for config availabilty before running deployment script

--- ewc_setup/main.sh
+++ ewc_setup/main.sh
@@ -9,10 +9,12 @@ echo
 
 chmod a+x $path_scripts/*.sh  # make sure all bash scripts are executable
 
+# check config can be read, otherwise exit without further action
+$path_scripts/get_config.sh || exit 2
+
 $path_scripts/update_python.sh
 $path_scripts/install_mwr_l12l2.sh
 $path_scripts/install_s3.sh
 $path_scripts/s3_create_buckets.sh eprofile-dl-raw eprofile-dl-l1  # comment this out if buckets alredy exist
 $path_scripts/s3_automount_buckets.sh eprofile-dl-raw eprofile-dl-l1
-
 
