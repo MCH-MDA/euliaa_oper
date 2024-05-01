d988352294f027f21d476ec8750330747c1978ab
rruefenacht <rruefenacht@users.noreply.github.com>
2024-05-01 15:00:29 +0200

    install dl-toolbox-runner instead of dl-toolbox itself

--- ewc_setup/install_dl_toolbox.sh
+++ ewc_setup/install_dl_toolbox.sh
@@ -1,7 +1,7 @@
 # clone dl_toolbox and install it with its dependencies. Needs pip>21.3 installed, hence first run update_python.sh
 
-repo_name=dl_toolbox_nrt
-repo_url=https://github.com/rruefenacht/dl_toolbox_nrt
+repo_name=dl_toolbox_runner
+repo_url=https://github.com/MeteoSwiss/dl_toolbox_runner.git
 base_dir="$HOME"  # directory where to install code in as a subdirectory dl_toolbox_nrt
 
 
