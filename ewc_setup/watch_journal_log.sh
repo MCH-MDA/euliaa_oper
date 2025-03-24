bf855533362051fe828008fe273b655e5f0a8565
Eric Sauvageat <47110383+leric2@users.noreply.github.com>
2025-03-24 15:32:39 +0100

    Update watch_journal_log.sh

--- ewc_setup/watch_journal_log.sh
+++ ewc_setup/watch_journal_log.sh
@@ -13,7 +13,7 @@ journalctl_output=$(journalctl --since="10 min ago" -u $SERVICE_NAME)
 if echo "$journalctl_output" | grep -q "No entries"; then
     restart_time=$(date "+%Y-%m-%d %H:%M:%S")
     echo "No entries found in journalctl logs in the last 10 min. Restarting $SERVICE_NAME at: $restart_time"
-    #sudo systemctl restart retrieval.service
+    sudo systemctl restart retrieval.service
     if [ $? -eq 0 ]; then
         echo "$SERVICE_NAME restarted successfully."
     else
