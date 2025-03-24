12b31695f5f06b572a4e29ad952de3a1a27140fb
Eric Sauvageat <47110383+leric2@users.noreply.github.com>
2025-03-24 16:27:18 +0100

    Update write_cron.sh

--- ewc_setup/write_cron.sh
+++ ewc_setup/write_cron.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # File writing the necessary cronjobs
+base_dir="$HOME"
 
 # Define the schedules and the commands to run
 cron_schedule_script="0,15,30,45 * * * *"
-script_path="/home/eric/eprofile_dl_oper/ewc_setup/watch_journal_log.sh"
-log_file="/home/eric/journal_watchdog.txt"
+script_path="$base_dir/ewc_setup/watch_journal_log.sh"
+log_file="$base_dir/journal_watchdog.txt"
 
 cron_schedule_restart="0 0 * * *"
 restart_command="/sbin/shutdown -r"
@@ -14,5 +15,6 @@ echo "$cron_schedule_script $script_path >> $log_file 2>&1" > mycron
 echo "$cron_schedule_restart $restart_command" >> mycron
 
 # Install the new cron jobs from the temporary file -> needs to be root for restart !
-sudo crontab mycron
-
+# sudo crontab mycron
+# Instead of installing the cron directly, output indication on how to do it:
+echo "cron can now be installed with sudo crontab mycron"
