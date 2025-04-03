fd8cfcce407a071ba450f16b321ec1bb3ffb4298
annecbroux <ac.billault-roux@wanadoo.fr>
2025-04-03 09:28:36 +0000

    Fixed install problems (shebang)

--- ewc_setup/main.sh
+++ ewc_setup/main.sh
@@ -5,26 +5,30 @@
 path_scripts=$(dirname "$0")
 
 echo 
-echo "setting up new VM from scratch - stay around, user input will be required"
+echo "SETTING UP NEW VM FROM SCRATCH - STAY AROUND, USER INPUT WILL BE REQUIRED"
 echo "=============================="
+echo
 echo
 
 chmod a+x $path_scripts/*.sh  # make sure all bash scripts are executable
 
 # check config can be read, otherwise exit without further action
-echo "Loading config..."
 $path_scripts/get_config.sh || exit $?
 
-echo "Python updates and environment creation..."
+echo "PYTHON UPDATES AND ENVIRONMENT CREATION"
+echo "=============================="
+echo
 $path_scripts/update_python.sh
 
-echo "Installing s3 buckets..."
+echo "INSTALLING S3 BUCKETS"
+echo "=============================="
+echo
 $path_scripts/install_s3.sh
 # $path_scripts/s3_create_buckets.sh eprofile-dl-raw eprofile-dl-l1  # comment this out if buckets alredy exist. Also, creating buckets in morpheus is preferrable (buckets created in command line don't show up in morpheus)
 $path_scripts/s3_automount_buckets.sh euliaa-test # mount buckets
 
+echo "DOWNLOAD AND INSTALL EULIAA PROC SCRIPTS"
+echo "=============================="
+echo
+source ~/.bashrc  # needed for proper poetry installation
 $path_scripts/install_euliaa_proc.sh # download and install processing scripts
-
-# OLD - E-PROFILE
-# $path_scripts/import_eprofile_config.sh
-# $path_scripts/write_cron.sh
